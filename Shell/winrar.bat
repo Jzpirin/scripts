87520d47b52f8d9f785f7ce8e8cef5f84492b39a
Jzpirin <may8thzhuhj@gmail.com>
2015-11-27 18:11:52 +0800

    update dbbackup bat scripts

--- Shell/winrar.bat
+++ Shell/winrar.bat
@@ -1,4 +1,9 @@
 set bakdate=%date:~0,4%%date:~5,2%%date:~8,2%
+set backuptime=%time%
+echo Backup Date:%bakdate%>D:\DBBAK\backup.log &echo Backup Time:%backuptime%>>D:\DBBAK\backup.log
 cd D:\TOOLS\SetupH3_Yun
+DebugTools.exe project=升级--分片A--备份数据 env=测试安装 show=1
+echo Backup Success and Begin Winrar>>D:\DBBAK\backup.log
 "D:\Program Files (x86)\WinRAR\WinRAR.exe" a D:\DBBAK\DBBAK_%bakdate%.rar dbbak
+echo ALL Success>>D:\DBBAK\backup.log
 pause
